8e446008c636fed72b773a1cfdc10f2fd545a2c6
Ladvix <ladvix.official@gmail.com>
2025-02-28 17:26:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+from googletrans import Translator, LANGUAGES
+TRANSLATOR = Translator()
+
+def launch(bot):
+    @bot.app.on_message(filters.command('tnt', prefixes='.') & filters.me)
+    def tnt(client, message):
+        data = message.text.split(' ', maxsplit=2)
+        text = data[2]
+        dest = data[1]
+
+        message.edit_text(TRANSLATOR.translate(text, dest = dest).text)
